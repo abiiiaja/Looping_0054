f4789f772e472583f1bdd8c66604337cbab72802
abiiiaja <kalindraaraya@gmail.com>
2026-03-31 18:00:04 +0700

    membuat program input

--- logikaNOT.cpp
+++ logikaNOT.cpp
@@ -4,4 +4,9 @@ using namespace std;
 int main(){
     float nilM, nilB, rerata;
     string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilB;
 }
