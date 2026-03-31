968bb90c698fc407b75dc08a26482b18635288a2
abiiiaja <kalindraaraya@gmail.com>
2026-03-31 18:21:24 +0700

    menampilkan status kelulusan

--- logikaOR.cpp
+++ logikaOR.cpp
@@ -12,9 +12,13 @@ int main() {
 
     rerata = (nilB + nilM) / 2;
 
-     if ((rerata >= 60) || (nilM >= 70)) {
+    if ((rerata >= 60) || (nilM >= 70)) {
         status = "Lulus";
     } else {
         status = "Tidak Lulus";
     }
+
+    cout << "Status kelulusan = " << status << ", dengan nilai rata rata = " << rerata << endl;
+
+    return 0;
 }
