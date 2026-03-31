ba1158706dcdc66a9c0c6d709504d007dd4ffecd
abiiiaja <kalindraaraya@gmail.com>
2026-03-31 17:44:34 +0700

    membuat perulangan FOR

--- looping.cpp
+++ looping.cpp
@@ -4,4 +4,12 @@ using namespace std;
 
 int main() {
     int x;
+
+    // Perulangan dengan FOR
+    // Mencetak tulisan sebanyak 5 kali
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Teknologi Informasi UNY" << endl;
+    }
+    cout << endl;
 }
